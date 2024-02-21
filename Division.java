5e79edae59df809f9b6df7a23ebf446507822ae6
Anusha Uppu <admin@Anusha-Uppu.local>
2024-02-21 13:09:08 +0530

    commiting

--- Division.java
+++ Division.java
@@ -2,20 +2,11 @@ import java.util.*;
 /**
  * Division
  */
- class addition {
-    public static int Addition(int a,int b){
-        return a+b;
-    }
-}
+
 public class Division {
 
     public static int  Divisionbytwo(int addResult){
-        addition aa=new addition();
-        int a,b;
-        Scanner sc=new Scanner(System.in);
-        a=sc.nextInt();
-        b=sc.nextInt();
-        aa.Addition(a, b);
+        
         return addResult/2;
     }
 }
